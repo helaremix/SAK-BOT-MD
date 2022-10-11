82efe3e2d8ca62eb1e73ed44f4432435de26bbdf
NIMA OFFICIAL <110774656+nimaofficial@users.noreply.github.com>
2022-10-11 13:23:17 +0530

    ⚜️SAK BOT MD⚜️

--- Sak.js
+++ Sak.js
@@ -90,7 +90,7 @@ async function startSakBotInc() {
         if (!SakBotInc.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
         m = smsg(SakBotInc, mek, store)
-        require("./Sak-Bot")(SakBotInc, m, chatUpdate, store)
+        require("./Sak-Bot.js")(SakBotInc, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
